1330b50a1c667fe6e2e00dfc813b142c0ff2925f
Sia2602 <danilojose26@hotmail.com>
2021-08-07 21:32:18 -0300

    Adicionando LineChart com mocks integrado.

--- Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
+++ Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../shared/Card/Card'
 import PieChart from '../mocks/pieOptions.json'
 import LineChart from '../mocks/lineOptions.json'
+import Api from '../mocks/api.json'
 import Charts from '../shared/Charts/Charts'
 import { ScrollingX, Wrapper, ScrollingY, Title } from './HomeView.styles'
 import { TeamOutlined } from '@ant-design/icons'
 
-class HomeView extends React.Component{   
+function HomeView(){
 
-    render(){
-        const widthCardMobile = 65
-        const widthCardDesktop = 20
-        const heightCard = 50    
-        const heightHeaderCard = 5
-        const heightBodyCard = this.heightCard-this.heightHeaderCard
-        
-        const chartPie = <div><Charts options={PieChart} /></div>
-        const chartLine = <div style={{width: '90vw'}}><Charts options={LineChart} /></div> 
-        
-        return <Wrapper>        
-            <Title>SETORES</Title>
+    const widthCardMobile = 65
+    const widthCardDesktop = 20
+    const heightCard = 50    
+    const heightHeaderCard = 5
+    const heightBodyCard = heightCard-heightHeaderCard
+    
+    const chartPie = <div><Charts options={PieChart} /></div>    
+    
+    const [api, setApi] = useState(Api)
+    const [dataLineChart, setdataLineChart] = useState(LineChart)
+    dataLineChart.series = api.unidades.map(unidade => unidade ? {name: unidade.name, data: [api.ativos.filter(ativo => ativo.unitId == unidade.id).length , api.usuarios.filter(usuario => usuario.unitId == unidade.id).length]} : {})
 
-            <ScrollingX>                
-                <Card title={"Thebrain"} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/> 
-                <Card title={"Hal"} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/> 
-                <Card title={"Multivac"} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/> 
-                <Card title={"Jaguar"} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/> 
-                <Card title={"Tobias"} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/> 
-            </ScrollingX>   
+    const chartLine = <div style={{width: '90vw'}}><Charts options={dataLineChart} /></div>
+    
+    return <Wrapper>        
+        <Title>SETORES</Title>
 
-            <ScrollingY>
-                {chartLine}
-            </ScrollingY>                
-        </Wrapper>
-    }
+        <ScrollingX>     
+            {api.unidades.map(unidade => <Card title={unidade.name} icon={<TeamOutlined/>} componentList={['Status dos Ativos', chartPie]} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightBodyCard={heightBodyCard} heightHeaderCard={heightHeaderCard}/>)}          
+        </ScrollingX>   
+
+        <ScrollingY>
+            {chartLine}
+        </ScrollingY>                
+    </Wrapper>
 }
 
 export default HomeView
