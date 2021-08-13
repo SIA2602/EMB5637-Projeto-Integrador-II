902e459ac11c5d73ce8f3ec7c1f3958f0aab234f
Sia2602 <danilojose26@hotmail.com>
2021-08-13 18:21:42 -0300

    Finalizando etapa de visualizacao dos usuarios para cada unidade.

--- Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
+++ Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
@@ -13,18 +13,17 @@ function HomeView(){
     const widthCardDesktop = 20
     const heightCard = 50    
     const heightHeaderCard = 5
-    const heightBodyCard = heightCard-heightHeaderCard   
+    const heightBodyCard = heightCard-heightHeaderCard     
     
-    const [api, setApi] = useState(Api)
+    const [api, setApi] = useState(Api)    
     const [dataLineChart, setdataLineChart] = useState(LineChart)        
     dataLineChart.series = api.unidades.map(unidade => unidade ? {name: unidade.name, data: [api.ativos.filter(ativo => ativo.unitId === unidade.id).length , api.usuarios.filter(usuario => usuario.unitId === unidade.id).length]} : {})
     const [listCards, setListCards] = useState(<Wrapper><Title>SETORES</Title>
-
             <ScrollingX>     
                 {api.unidades.map(unidade => <Card title={unidade.name}
                     icon={<TeamOutlined/>}
-                    onToggle={toggleUsers}
-                    listaUsers={api.usuarios}
+                    onToggle={togglePageUsers}                    
+                    lista={api.usuarios.filter(usuario => usuario.unitId === unidade.id)}                                      
                     componentList={['Status dos Ativos', <Charts options={getPieChart(unidade, api)} />]}
                     widthCardMobile={widthCardMobile}
                     widthCardDesktop={widthCardDesktop}
@@ -33,35 +32,54 @@ function HomeView(){
                     heightHeaderCard={heightHeaderCard}
                 />)}                   
             </ScrollingX>   
-
             <ScrollingY>
                 <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
             </ScrollingY>                
         </Wrapper>      
-    )   
+    )      
 
-    function toggleUsers(listUsers){
+    function togglePageUsers(listUsers){
         const newCards = <Wrapper> <Title>USUARIOS</Title>
+            <ScrollingX>     
+                {listUsers.map(user => <Card title={user.name}
+                icon={<RollbackOutlined />}
+                onToggle={togglePageHome}
+                lista={api.unidades}                                    
+                componentList={['Status dos Ativos']}
+                widthCardMobile={widthCardMobile}
+                widthCardDesktop={widthCardDesktop}
+                heightCard={heightCard}
+                heightBodyCard={heightBodyCard}
+                heightHeaderCard={heightHeaderCard}
+            />)}                  
+            </ScrollingX>  
+            <ScrollingY>
+                <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
+            </ScrollingY>                
+        </Wrapper>          
+        setListCards(newCards)              
+    }
 
-                <ScrollingX>     
-                    {listUsers.map(user => <Card title={user.name}
-                    icon={<RollbackOutlined />}
-                    componentList={['Status dos Ativos']}
+    function togglePageHome(unidades){
+        const newCards = <Wrapper><Title>SETORES</Title>
+            <ScrollingX>     
+                {unidades.map(unidade => <Card title={unidade.name}
+                    icon={<TeamOutlined/>}
+                    onToggle={togglePageUsers}                    
+                    lista={api.usuarios.filter(usuario => usuario.unitId === unidade.id)}                                    
+                    componentList={['Status dos Ativos', <Charts options={getPieChart(unidade, api)} />]}
                     widthCardMobile={widthCardMobile}
                     widthCardDesktop={widthCardDesktop}
                     heightCard={heightCard}
                     heightBodyCard={heightBodyCard}
                     heightHeaderCard={heightHeaderCard}
-                />)}                  
-                </ScrollingX>   
-
-                <ScrollingY>
-                    <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
-                </ScrollingY>                
-            </Wrapper>
-             
-        
-        setListCards(newCards)        
+                />)}                   
+            </ScrollingX>  
+            <ScrollingY>
+                <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
+            </ScrollingY>                
+        </Wrapper>          
+        setListCards(newCards)              
     }
 
     return listCards
--- Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/utils/getPieChart.js
+++ Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/utils/getPieChart.js
@@ -1,7 +1,7 @@
 import PieChart from '../mocks/pieOptions.json'
 
 export default function getPieChart(unidade, api){    
-    var pie = JSON.parse(JSON.stringify(PieChart));      
+    var pie = JSON.parse(JSON.stringify(PieChart));     
     
     pie.series[0].data[0].y = api.ativos.filter(ativo => ativo ? ativo.unitId === unidade.id && ativo.status === "inOperation" : {}).length
     pie.series[0].data[1].y = api.ativos.filter(ativo => ativo ? ativo.unitId === unidade.id && ativo.status === "inAlert" : {}).length
