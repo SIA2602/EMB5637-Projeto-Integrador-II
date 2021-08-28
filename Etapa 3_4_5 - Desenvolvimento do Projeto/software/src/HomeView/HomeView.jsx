8ef88318ec1a0d72cbcd1b848deeca34246d9850
Sia2602 <danilojose26@hotmail.com>
2021-08-28 10:54:42 -0300

    Adicionando recurso de consultar os ativos. Deixando o sofware como a versao base.

--- Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
+++ Etapa 3_4_5 - Desenvolvimento do Projeto/software/src/HomeView/HomeView.jsx
@@ -1,58 +1,15 @@
-/*import React, { useState, useEffect } from 'react'
-import LineChart from '../mocks/lineOptions.json'
-import Api from '../mocks/api.json'
-import 'antd/dist/antd.css'
-import axios from 'axios'
-import TogglePageHome from '../HomeView/TogglePageHome'
-import TogglePageUsers from '../HomeView/TogglePageUsers'
-import TogglePageAtivos from '../HomeView/TogglePageAtivos'
-
-function HomeView(){
-
-    const widthCardMobile = 65
-    const widthCardDesktop = 20
-    const heightCard = 50    
-    const heightHeaderCard = 5
-    const heightBodyCard = heightCard-heightHeaderCard     
-    
-    const [api, setApi] = useState(Api)    
-    console.log(api)      
-    const [dataLineChart, setdataLineChart] = useState(LineChart)        
-    dataLineChart.series = api.unidades.map(unidade => unidade ? {name: unidade.name, data: [api.ativos.filter(ativo => ativo.unitId === unidade.id).length , api.usuarios.filter(usuario => usuario.unitId === unidade.id).length]} : {})
-    const [listCards, setListCards] =  useState(<TogglePageHome api={api} dataLineChart={dataLineChart} togglePageUsers={togglePageUsers} togglePageAtivos={togglePageAtivos} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightHeaderCard={heightHeaderCard} heightBodyCard={heightBodyCard}/>)    
-
-    function togglePageUsers(unidadeId){            
-        setListCards(<TogglePageUsers api={api} listUsers={api.usuarios.filter(usuario => usuario.unitId === unidadeId)} dataLineChart={dataLineChart} togglePageHome={togglePageHome} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightHeaderCard={heightHeaderCard} heightBodyCard={heightBodyCard}/>)              
-    }
-
-    function togglePageHome(){    
-        console.log(api)             
-        setListCards(<TogglePageHome api={api} dataLineChart={dataLineChart} togglePageUsers={togglePageUsers} togglePageAtivos={togglePageAtivos} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightHeaderCard={heightHeaderCard} heightBodyCard={heightBodyCard}/>)              
-    }
-
-    function togglePageAtivos(unidadeId){                 
-        setListCards(<TogglePageAtivos api={api} listAtivos={api.ativos.filter(ativo => ativo.unitId === unidadeId)} dataLineChart={dataLineChart} togglePageHome={togglePageHome} widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} heightCard={heightCard} heightHeaderCard={heightHeaderCard} heightBodyCard={heightBodyCard}/>)              
-    }   
-
-    useEffect(() => {         
-        setInterval(() => axios.get('https://e931-186-232-38-178.ngrok.io/api').then(response => setApi(response.data)), 5000)        
-    }, []);  
-
-    return listCards 
-}
-
-export default HomeView*/
-
 import React, { useState, useEffect } from 'react'
 import LineChart from '../mocks/lineOptions.json'
-//import Api from '../mocks/api.json'
+//import Api from '../mocks/api.json' //mock da api utilizada
 import 'antd/dist/antd.css'
 import axios from 'axios'
 import Card from '../shared/Card/Card'
 import getPieChart from '../utils/getPieChart'
 import Charts from '../shared/Charts/Charts'
-import { ScrollingX, Wrapper, ScrollingY, Title, WrapperLineChart, SpaceText, ImgProfile, LineSeparator, ListTextUsers } from './HomeView.styles'
-import { TeamOutlined, RollbackOutlined } from '@ant-design/icons'
+import { ScrollingX, Wrapper, ScrollingY, Title, WrapperLineChart, SpaceText, ImgProfile, LineSeparator, ListTextUsers, Buttom, ImgMachine } from './HomeView.styles'
+import { TeamOutlined, RollbackOutlined, PlusCircleOutlined } from '@ant-design/icons'
+import { Alert } from 'antd'
+import getStringStatus from '../utils/getStringStatus'
 
 function HomeView(){
 
@@ -64,7 +21,7 @@ function HomeView(){
     const heightBodyCard = heightCard-heightHeaderCard     
 
     //URL da api
-    const urlAPI = 'https://0414-186-232-38-178.ngrok.io/api'
+    const urlAPI = ' https://921d-186-232-34-75.ngrok.io/api'
     
     const [api, setApi] = useState({
         "ativos":[],
@@ -79,7 +36,7 @@ function HomeView(){
         <ScrollingX>     
             {api.unidades.map(unidade => <Card title={unidade.name}
                 icon={<TeamOutlined/>}                                                    
-                componentList={[ <SpaceText>Status dos Ativos</SpaceText>, <Charts options={getPieChart(unidade, api)} />]}
+                componentList={[ <SpaceText>Status dos Ativos </SpaceText>, <Charts options={getPieChart(unidade, api)} />]}
                 setPageHomeToUsers={setPageHomeToUsers}
                 usuarios={[]}
                 widthCardMobile={widthCardMobile}
@@ -120,7 +77,7 @@ function HomeView(){
                 <ScrollingX>     
                     {api.unidades.map(unidade => <Card title={unidade.name}
                         icon={<TeamOutlined/>}                                                    
-                        componentList={[ <SpaceText>Status dos Ativos</SpaceText>, <Charts options={getPieChart(unidade, api)} />]}
+                        componentList={[ <SpaceText>Status dos Ativos <Buttom onClick={() => setPageHomeToAtivos(api.ativos.filter(ativo => ativo.unitId === unidade.id))}><PlusCircleOutlined /></Buttom></SpaceText>, <Charts options={getPieChart(unidade, api)} />]}
                         setPageHomeToUsers={setPageHomeToUsers}
                         usuarios={api.usuarios.filter(usuario => usuario.unitId === unidade.id)}
                         widthCardMobile={widthCardMobile}
@@ -142,8 +99,7 @@ function HomeView(){
                 <ScrollingX>     
                     {indexUnit.map(user => <Card title={user.name}
                     icon={<RollbackOutlined />}
-                    setPageHomeToUsers={setPageHomeToUsers}        
-                    unidade={null}                                          
+                    setPageHomeToUsers={setPageHomeToUsers}                                                          
                     componentList={[<ImgProfile src={user.imgProfile} />, <LineSeparator widthCardDesktop={widthCardDesktop} widthCardMobile={widthCardMobile}/>, <ListTextUsers widthCardDesktop={widthCardDesktop} widthCardMobile={widthCardMobile}><SpaceText>{user.email}</SpaceText>{user.foneNumber}</ListTextUsers>, <LineSeparator widthCardDesktop={widthCardDesktop} widthCardMobile={widthCardMobile}/>]}
                     widthCardMobile={widthCardMobile}
                     widthCardDesktop={widthCardDesktop}
@@ -156,7 +112,28 @@ function HomeView(){
                     <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
                 </ScrollingY>                
             </Wrapper>) 
-        }               
+        }   
+        
+        //Caso em 2 sera mostrado a pagina referente aos dados dos ativos daquela unidade
+        else if(pageAplication === 2 && indexUnit !== null){
+            setListCards(<Wrapper> <Title>ATIVOS</Title>
+                <ScrollingX>     
+                    {indexUnit.map(ativo => <Card title={ativo.name}
+                    icon={<RollbackOutlined />}
+                    setPageHomeToUsers={setPageHomeToAtivos}                                                        
+                    componentList={[<ImgMachine widthCardMobile={widthCardMobile} widthCardDesktop={widthCardDesktop} src={ativo.image} />, <Alert message={<SpaceText>{ativo.status} <Buttom ><PlusCircleOutlined /></Buttom></SpaceText>} type={getStringStatus(ativo.status)} showIcon />]}
+                    widthCardMobile={widthCardMobile}
+                    widthCardDesktop={widthCardDesktop}
+                    heightCard={heightCard}
+                    heightBodyCard={heightBodyCard}
+                    heightHeaderCard={heightHeaderCard}
+                />)}                  
+                </ScrollingX>  
+                <ScrollingY>
+                    <WrapperLineChart><Charts options={dataLineChart} /></WrapperLineChart>
+                </ScrollingY>                
+            </Wrapper>) 
+        }
     }, [dataLineChart])   
     
     //Funcao responsavel por modificar o estado da pagina home para pagina de usuarios ou vise versa
@@ -164,6 +141,18 @@ function HomeView(){
         if(pageAplication === 0){
             setIndexUnit(usuarios)
             setPageAplication(1)            
+        } 
+        else{
+            setPageAplication(0)
+            setIndexUnit(null)
+        }           
+    }
+
+    //Funcao responsavel por modificar o estado da pagina home para pagina de ativos ou vise versa
+    function setPageHomeToAtivos(ativos){        
+        if(pageAplication === 0){
+            setIndexUnit(ativos)
+            setPageAplication(2)            
         } 
         else{
             setPageAplication(0)
